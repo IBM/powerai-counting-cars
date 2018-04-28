82bdb2ff26ff5d8c5da496115b3937e20aacf1b1
Mark Sturdevant <mark.sturdevant@ibm.com>
2018-04-27 17:52:33 -0700

    Counting cars notebook
    Count cars in a video using a Jupyter Notebook.
    Getting this repo re-started with a notebook solution
    for counting cars in a video.
    
    * Still plenty of TODOs.

---
+++ ACKNOWLEDGEMENTS.md
@@ -0,0 +1,5 @@
+## Acknowledgements
+
+* Credit goes to Michael Hollinger for his initial notebook counting objects with the PowerAI Vision Video Data Platform.
+* Credit goes to Ke Wei Sun for code and ideas that were leveraged to enhance the notebook for car counting.
+
---
+++ tools/create_video.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -e
+# Copyright 2018 IBM All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ $# -eq 0 ]; then
+    echo "Usage error: Specify the output directory as arg1."
+fi
+
+ffmpeg -r 60 -f image2 -i "$1/output-frame_%05d.jpg" -vcodec libx264 -crf 25  -pix_fmt yuvj420p "$1/output-video.mp4"
+
+echo "Video written to \"$1/output-video.mp4\""
